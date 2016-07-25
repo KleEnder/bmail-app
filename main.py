a93239440ccf40847905b673fabf73f800901ec8
Klemen Znidar <klemenznidar208@gmail.com>
2016-07-25 13:31:48 +0200

    Finished weather part

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import jinja2
 import webapp2
 import json
+from google.appengine.api import users
 from google.appengine.api import urlfetch
 
 template_dir = os.path.join(os.path.dirname(__file__), "templates")
@@ -30,27 +31,57 @@ class BaseHandler(webapp2.RequestHandler):
 
 class MainHandler(BaseHandler):
     def get(self):
-        data = open("people.json", "r").read()
-        json_data = json.loads(data)
+        user = users.get_current_user()
 
-        params = {"seznam": json_data}
+        if user:
+            prijavljen = True
+            logout_url = users.create_logout_url('/')
 
-        self.render_template("hello.html", params)
+            params = {"prijavljen": prijavljen, "logout_url": logout_url, "user": user}
+        else:
+            prijavljen = False
+            login_url = users.create_login_url('/')
+
+            params = {"prijavljen": prijavljen, "login_url": login_url, "user": user}
+
+        return self.render_template("hello.html", params=params)
 
 class WeatherHandler(BaseHandler):
     def get(self):
-        url = "http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=555d2db65137ecbbd820613da4006d3d"
+        url = "http://api.openweathermap.org/data/2.5/weather?q=Ljubljana,svn&appid=555d2db65137ecbbd820613da4006d3d"
 
         result = urlfetch.fetch(url)
 
-        podatki = json.loads(result.content)
+        url_podatki = json.loads(result.content)
 
-        params = {"podatki": podatki}
+        params = {"podatki": url_podatki}
 
-        self.render_template("vreme.html", params)
+        user = users.get_current_user()
+
+        params["user"] = user
+
+        if user:
+            prijavljen = True
+            logout_url = users.create_logout_url('/')
+
+            params["prijavljen"] = prijavljen
+            params["logout_url"] = logout_url
+        else:
+            prijavljen = False
+            login_url = users.create_login_url('/')
+
+            params["prijavljen"] = prijavljen
+            params["login_url"] = login_url
+
+        return self.render_template("vreme.html", params=params)
 
 
 app = webapp2.WSGIApplication([
     webapp2.Route('/', MainHandler),
     webapp2.Route('/vreme', WeatherHandler)
 ], debug=True)
+'''
+data = open("people.json", "r").read()
+json_data = json.loads(data)
+params["seznam"] = [json_data]
+'''
